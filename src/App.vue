80f889ea6744cc26db287605fee833ca6cc31382
guoxiuxin <3591311930@qq.com>
2022-09-08 17:48:07 +0800

    代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <div>sjhadhjsg1</div>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { GetConstellationData } from "./config/api/user";
+GetConstellationData();
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { store } from "./store/index"
+import "./mock/user"
 
-createApp(App).mount('#app')
+createApp(App).use(store).mount('#app')
