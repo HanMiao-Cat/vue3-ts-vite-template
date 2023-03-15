f13d4aa90f6ac5fc4a7b8dae89ea2cc306d592ff
gxx <1786232520@qq.com>
2023-03-15 19:22:54 +0800

    style: eslint等代码格式统一

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,40 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ArcoResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 8080,
     open: true,
     proxy: {
-      "/api": {
-        target: "http://gateway.wubangtu.xyz:9999",
+      '/api': {
+        target: 'http://gateway.wubangtu.xyz:9999',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/\/api/, ""),
+        rewrite: (path) => path.replace(/\/api/, ''),
       },
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   plugins: [
     vue(),
+    eslintPlugin({
+      include: [
+        'src/**/*.ts',
+        'src/**/*.vue',
+        'src/**/*.jsx',
+        'src/*.ts',
+        'src/*.vue',
+        'src/*.jsx',
+      ],
+    }),
     AutoImport({
       resolvers: [ArcoResolver()],
     }),
